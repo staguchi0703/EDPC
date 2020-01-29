9b537cf8b5f46d35f4cd55de808c5e188ba98d42
sho Taguchi <s.taguchi0703@gmail.com>
2020-01-29 23:22:20 +0900

    BがpythonだとTLE

--- B/B.py
+++ B/B.py
@@ -16,6 +16,16 @@ sys.stdin=f
 ##################################
 # %%
 # 以下ペースト可
-num_list = [int(item) for item in input().split()]
-print('B', num_list)
+N, K = [int(item) for item in input().split()]
+h =[0] + [int(item) for item in input().split()]
 
+dp = [0 for _ in range(N+1)]
+
+for i in range(N+1):
+    for k in range(1, K+1):
+        if i - k >= 1:
+            if dp[i] != 0:
+                dp[i] = min(dp[i-k] + abs(h[i] - h[i-k]), dp[i])
+            else:
+                dp[i] = dp[i-k] + abs(h[i] - h[i-k])
+print(dp[N])
