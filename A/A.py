1db5f0f9248076e5856c2d4ab65e4a2ae55873e9
Sho Taguchi <s.taguchi703@gmail.com>
2020-01-31 22:45:21 +0900

    Aをnumpyで

--- A/A.py
+++ A/A.py
@@ -16,20 +16,22 @@ sys.stdin=f
 ##################################
 # %%
 # 以下ペースト可
+import sys
+input = sys.stdin.readline
+import numpy as np
+
 N = int(input())
-hight_list = [int(item) for item in input().split()]
+hight_list = np.array([int(item) for item in input().split()])
 
-
-dp = [0 for i in range(N+1)]
-
+dp = np.full(N, hight_list.sum(), dtype='int64')
+dp[0] = 0
 dp[1] = abs(hight_list[1] - hight_list[0])
 
 for i in range(2, N):
-    jump1 = abs(hight_list[i] - hight_list[i-1])
-    jump2 = abs(hight_list[i] - hight_list[i-2])
+    cost1 = abs(hight_list[i-1] - hight_list[i])
+    cost2 = abs(hight_list[i-2] - hight_list[i])
+    dp[i] = min(dp[i-1] + cost1, dp[i-2] + cost2)
+    # print(dp)
 
-    dp[i] = min(dp[i-1] + jump1, dp[i-2] + jump2)
-
-
-print(dp[N-1])
+print(np.max(dp))
     
